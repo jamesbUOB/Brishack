fb923f8ae7407cd2f0627a074c8e65b135265a0c
kaungzhan <kaungzawhan2000@gmail.com>
2025-02-16 18:57:18 +0000

    images added

--- app/lib/screens/introduction.dart
+++ app/lib/screens/introduction.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'home.dart'; 
+import 'home.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-
 class Introduction extends StatefulWidget {
+  const Introduction({Key? key}) : super(key: key); // Added constructor
 
   @override
   _IntroductionState createState() => _IntroductionState();
-
 }
+
 class _IntroductionState extends State<Introduction> {
   late IO.Socket socket;
   String connectionStatus = 'Disconnected';
@@ -58,10 +58,7 @@ class _IntroductionState extends State<Introduction> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    //    appBar: AppBar(
-        // title: Text('Introduction Screen'),
-      // ),
-       body: Container(
+      body: Container(
         width: double.infinity,
         height: double.infinity,
         decoration: const BoxDecoration(
@@ -69,22 +66,51 @@ class _IntroductionState extends State<Introduction> {
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              //Color(0xFF4CAF50), // Green
-
-              Color(0xFF8D6E63), 
-              Color(0xFF4CAF50),
-              // Earthy Brown
+              Color(0xFF8D6E63), // Earthy Brown
+              Color(0xFF4CAF50), // Green
             ],
           ),
         ),
-      child: Center(
-        child: ElevatedButton(
-          onPressed: () {
-          socket.emit('message', 'start');
-          },
-          child: const Text('Start simulation'),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Center( // Wrap button in Center for better layout
+                child: ElevatedButton(
+                  onPressed: () {
+                    socket.emit('message', 'start');
+                  },
+                  child: const Text('Start simulation'),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.all(20),
+                  height: 100,))),
+                  SliverToBoxAdapter(child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.all(20),))),
+                  SliverToBoxAdapter(child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.all(20),)))
+          ],
         ),
-      ),),);
-    
+      ),
+    );
   }
 }

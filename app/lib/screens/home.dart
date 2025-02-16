9603041e3241ec789ca83ac0d7266eef8a9a1b6f
kaungzhan <kaungzawhan2000@gmail.com>
2025-02-16 12:59:22 +0000

    starting a scrollable main page

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -2,52 +2,49 @@ import 'package:flutter/material.dart';
 import 'introduction.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: Container(
-                width: double.infinity,
-                height: double.infinity,
-            decoration: const BoxDecoration(
-            gradient: LinearGradient(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
             begin: Alignment.bottomCenter,
             end: Alignment.topCenter,
             colors: [
-            Color(0xFF8D6E63), // Earthy Brown
-            Color(0xFF4CAF50), // Green
+              Color(0xFF8D6E63), // Earthy Brown
+              Color(0xFF4CAF50), // Green
             ],
-            ),
+          ),
         ),
-        
-        // Display Homepage Content
-          child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              const Text(
-                "What will you discover?",
-                style: TextStyle(fontSize: 18, color: Colors.white),
+        child: const CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'What will you discover?',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Add your onPressed code here!
-                  Navigator.of(context).push(
-                     MaterialPageRoute(builder: (context) =>  Introduction()),
-                  );
-                },
-                child: const Text("Explore"),
-
+            ),
             
-        ),
-          ],))),
-          );
-
-    }
+            SliverToBoxAdapter(
+              
+            )
+                  ],
+                ),
+              ),
+    );
+  }
 }

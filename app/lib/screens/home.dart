1591e697ae3083e263bb2e1360cfc869e2f4ff56
elsawakfi <xp23980@bristol.ac.uk>
2025-02-16 12:36:34 +0000

    comiting

--- app/lib/screens/home.dart
+++ app/lib/screens/home.dart
@@ -29,6 +29,8 @@ class _HomePageState extends State<HomePage> {
           child: Container(
           padding: const EdgeInsets.all(20),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const Text(
                 "What will you discover?",

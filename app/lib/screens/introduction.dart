5403b09511afb5ed0eeaf7911c6ad15512388e8b
elsawakfi <xp23980@bristol.ac.uk>
2025-02-16 15:02:53 +0000

    Fixed the background of the introduction page

--- app/lib/screens/introduction.dart
+++ app/lib/screens/introduction.dart
@@ -10,20 +10,37 @@ class _IntroductionState extends State<Introduction> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+       appBar: AppBar(
         title: Text('Introduction Screen'),
       ),
-      body: Center(
+       body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              //Color(0xFF4CAF50), // Green
+
+              Color(0xFF8D6E63), 
+              Color(0xFF4CAF50),
+              // Earthy Brown
+            ],
+          ),
+        ),
+      child: Center(
         child: ElevatedButton(
           onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => HomePage()),
             );
+            
           },
           child: Text('Back to Home Screen'),
         ),
-      ),
-    );
+      ),),);
+    
   }
 }

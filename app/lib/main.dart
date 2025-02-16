7a850af2a55073bce9612314e0ad5586d05e1f3f
ZakiyaY <hg23195@bristol.ac.uk>
2025-02-16 22:27:39 +0000

    removed debug symbol

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -114,6 +114,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }

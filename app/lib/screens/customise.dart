a6be3bc0830a566cdb1d0f50f23d269cc3e5601a
ZakiyaY <hg23195@bristol.ac.uk>
2025-02-17 09:10:01 +0000

    Styling custom page

--- app/lib/screens/customise.dart
+++ app/lib/screens/customise.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
+
 class CustomisePage extends StatefulWidget {
   const CustomisePage({super.key});
 
@@ -52,13 +53,12 @@ class _CustomisePageState extends State<CustomisePage> {
     socket.disconnect();
     super.dispose();
   }
- List<bool> choices = <bool>[ false,false,false ];
+
+  List<bool> choices = <bool>[false, false, false];
   @override
   Widget build(BuildContext context) {
-   
-
     return Scaffold(
-       body: Container(
+      body: Container(
         width: double.infinity,
         height: double.infinity,
         decoration: const BoxDecoration(
@@ -71,7 +71,6 @@ class _CustomisePageState extends State<CustomisePage> {
             ],
           ),
         ),
-    
         child: CustomScrollView(
           slivers: [
             // **Title & Introduction**
@@ -83,7 +82,10 @@ class _CustomisePageState extends State<CustomisePage> {
                   children: [
                     const Text(
                       "🌍 Shape Bristol’s Urban Wild: Your Choice Matter",
-                      style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: Colors.white),
+                      style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
                     const SizedBox(height: 10),
                     const Text(
@@ -111,7 +113,7 @@ class _CustomisePageState extends State<CustomisePage> {
             // **Air Pollution Customization**
             buildToggleCard(
               "Air Pollution 🌫️",
-              "The city air changes every day. Will it remain fresh, or will it challenge those who breathe it?",
+              "Increase or decrease pollution levels - will the city air stay fresh, or will it become a challenge for those who breathe it?",
               choices[1],
               (bool value) {
                 setState(() {
@@ -139,13 +141,19 @@ class _CustomisePageState extends State<CustomisePage> {
                 child: Center(
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 15),
                       backgroundColor: const Color.fromARGB(255, 15, 114, 18),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                     onPressed: () {
+                      var sent_data = {
+                        "waste": choices[0],
+                        "mist": choices[1],
+                        "urban": choices[2]
+                      };
                       // Send a test message to the server
                       socket.emit('message', 'start');
                     },
@@ -161,7 +169,8 @@ class _CustomisePageState extends State<CustomisePage> {
             // **Navigation Buttons (Back & Next)**
             SliverToBoxAdapter(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 20.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -170,25 +179,31 @@ class _CustomisePageState extends State<CustomisePage> {
                         Navigator.pop(context);
                       },
                       icon: const Icon(Icons.arrow_back, color: Colors.white),
-                      label: const Text('Back', style: TextStyle(color: Colors.white)),
+                      label: const Text('Back',
+                          style: TextStyle(color: Colors.white)),
                       style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 13, 87, 15)),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color.fromARGB(255, 13, 87, 15)),
                       ),
                     ),
                     ElevatedButton(
                       onPressed: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => const CustomisePage()),
+                          MaterialPageRoute(
+                              builder: (context) => const CustomisePage()),
                         );
                       },
                       style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 13, 87, 15)),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color.fromARGB(255, 13, 87, 15)),
                       ),
                       child: Row(
                         children: const [
                           Text('Next', style: TextStyle(color: Colors.white)),
-                          SizedBox(width: 5), // Add some space between the text and the icon
+                          SizedBox(
+                              width:
+                                  5), // Add some space between the text and the icon
                           Icon(Icons.arrow_forward, color: Colors.white),
                         ],
                       ),
@@ -204,7 +219,8 @@ class _CustomisePageState extends State<CustomisePage> {
   }
 
   // **Reusable Widget for Customization Toggles**
-  Widget buildToggleCard(String title, String description, bool value, Function(bool) onChanged) {
+  Widget buildToggleCard(
+      String title, String description, bool value, Function(bool) onChanged) {
     return SliverToBoxAdapter(
       child: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -212,7 +228,9 @@ class _CustomisePageState extends State<CustomisePage> {
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(20),
-            boxShadow: [BoxShadow(color: Colors.black26, blurRadius: 4)], // Subtle shadow for depth
+            boxShadow: [
+              BoxShadow(color: Colors.black26, blurRadius: 4)
+            ], // Subtle shadow for depth
           ),
           padding: const EdgeInsets.all(20),
           child: Column(
@@ -220,7 +238,8 @@ class _CustomisePageState extends State<CustomisePage> {
             children: [
               Text(
                 title,
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 5),
               Text(
